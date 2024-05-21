05aa716b2be4fe17431dfeb65b390e15e43192cf
DheerajPatil42 <121562679+DheerajPatil42@users.noreply.github.com>
2024-05-21 10:14:20 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,7 +5,7 @@ class hello
         System.out.println("My First Java Program.");
         System.out.println("Commit1");
         System.out.println("Commit2");
-         System.out.println("Commit3");
+        
         
        
     }
